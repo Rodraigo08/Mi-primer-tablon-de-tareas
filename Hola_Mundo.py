cf68de30a731ec9a0fe7f5941a5a1fbc1ba28431
Rodraigo08 <81405470+Rodraigo08@users.noreply.github.com>
2026-06-14 08:12:40 -0600

    Mi primer script de tareas

---
+++ Hola_Mundo.py
@@ -0,0 +1,7 @@
+print("PROGRAMA PAR MI KOLALOKA \n\n (Tienes que escribir lo que se te pide y en cuanto acabes presionas enter)")
+t=input ("Pon nombre a una tarea:   ")
+n=input ("Ahora dime para quien es esta tarea:   ")
+p=input ("Ahora cuantos puntos va a tener esta tarea:   ")
+
+
+print("Ok entonces esta tarea es para: ", n ," y tiene que hacer: ",t," la cual tiene un valor de: ",p)
