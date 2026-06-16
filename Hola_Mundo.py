f77eb95e8ff319fc3134b82baabf19f2c27ba8c5
Rodraigo08 <81405470+Rodraigo08@users.noreply.github.com>
2026-06-16 03:52:24 -0600

    Actualizar Hola_Mundo.py

--- Hola_Mundo.py
+++ Hola_Mundo.py
@@ -1,9 +1,20 @@
-print("PROGRAMA PAR MI KOLALOKA \n\n (Tienes que escribir lo que se te pide y en cuanto acabes presionas enter)")
-t=input ("Pon nombre a una tarea:   ")
-n=input ("Ahora dime para quien es esta tarea:   ")
-p=input ("Ahora cuantos puntos va a tener esta tarea:   ")
-if t!="" and n!="" and p!="":
+print("PROGRAMA DE ESTUDIO \n\n (Tienes que escribir lo que se te pide y en cuanto acabes presionas enter)")
+t=input ("Pon nombre a una tarea:   ")# Pon una pequeña descripcion de la tarea a realizar
+n=input ("Ahora dime para quien es esta tarea:   ")# Introduce quien es la persona que debe de hacer esta tarea
+
+while True:
+    p_input = input("Ahora cuantos puntos va a tener esta tarea:")#ibtroduce los puntos que vaya a tenwr esta tarea
+    
+    try:
+        # Intenta convertir la entrada a un número entero
+        p = int(p_input) 
+        break  # Si lo logra, sale del bucle
+    except ValueError:
+        # Si el usuario puso una letra, se ejecuta esto en vez de crashear
+        print("¡Error! Por favor, introduce un número válido, no una letra.")#Error pusiste un carracter erroneo 
+        
+if t!="" and n!="" and p!=0:# verificador de datos
 
     print("Ok entonces esta tarea es para: ", n ," y tiene que hacer: ",t," la cual tiene un valor de: ",p)
 else:
-    print("No llenaste bien los datos requeridos" )
+    print("No llenaste bien los datos requeridos")
