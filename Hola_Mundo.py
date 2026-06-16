a61532a0e8ff2991e0d1edbbdfb2301e3f55ab77
Rodraigo08 <81405470+Rodraigo08@users.noreply.github.com>
2026-06-15 21:49:45 -0600

    Actualizar Hola_Mundo.py

--- Hola_Mundo.py
+++ Hola_Mundo.py
@@ -2,8 +2,8 @@ print("PROGRAMA PAR MI KOLALOKA \n\n (Tienes que escribir lo que se te pide y en
 t=input ("Pon nombre a una tarea:   ")
 n=input ("Ahora dime para quien es esta tarea:   ")
 p=input ("Ahora cuantos puntos va a tener esta tarea:   ")
-if t=="" or n=="" or p=="" 
+if t!="" and n!="" and p!="":
 
     print("Ok entonces esta tarea es para: ", n ," y tiene que hacer: ",t," la cual tiene un valor de: ",p)
-else
+else:
     print("No llenaste bien los datos requeridos" )
